ccee4573d906ffaf9cc226f42355be4329631012
bilalnahas12 <Bilal3a9ad@gmail.com>
2024-03-02 22:22:48 +0100

    caledar

--- lib/homepage/viewpage/pagecaledar.dart
+++ lib/homepage/viewpage/pagecaledar.dart
@@ -1,9 +1,25 @@
+import 'package:calendar_slider/calendar_slider.dart';
 import 'package:flutter/material.dart';
+
 class pagecaledar extends StatelessWidget {
   const pagecaledar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Container(child: Text("caledar"),));
+    return Column(
+      children: [
+        CalendarSlider(
+          initialDate: DateTime.now(),
+          firstDate: DateTime.now().subtract(Duration(days: 140)),
+          lastDate: DateTime.now().add(Duration(days: 4)),
+          selectedDateColor: Colors.white,
+          selectedTileBackgroundColor: Colors.black,
+          selectedDayPosition: SelectedDayPosition.left,
+          onDateSelected: (date) {
+            print(date);
+          },
+        ),
+      ],
+    );
   }
 }
