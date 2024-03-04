e1c3edf11da7fa8986c34aab7d5908f06f0a171f
bilalnahas12 <Bilal3a9ad@gmail.com>
2024-03-04 13:55:42 +0100

    conect firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:timesheet/homepage/indexpage.dart';
 import 'package:timesheet/login/login.dart';
 import 'package:timesheet/login/sigin.dart';
 
+import 'login/Autologin.dart';
 import 'weget/button_name.dart';
 import 'weget/imput.dart';
 
-void main() {
+void main() async{
+   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -15,6 +19,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: HomePage());
+        home:Auth());
   }
 }
